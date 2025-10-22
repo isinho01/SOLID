d1d5b5dd9ab7976847e70494ff1e4cddec591d7f
Isaac Oliveira <135730107+isinho01@users.noreply.github.com>
2025-10-22 11:32:13 -0300

    Atualização

--- atividade-simplificada/PixPagamento.java
+++
@@ -1,8 +0,0 @@
-class PixPagamento{
-    public void processarpix{
-         if (pedido.getTipoPagamento().equals("pix")) {
-            System.out.println("Processando pagamento via Pix...");
-            // Lógica específica para Pix
-        } 
-    }
-}
